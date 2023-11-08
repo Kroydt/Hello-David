a8f2ded2a5cf3a3bf971a678aa2f45387b50ead3
Kroydt <150144698+Kroydt@users.noreply.github.com>
2023-11-08 08:50:59 +0100

    Delete Hello David directory

--- Hello David/script.js
+++
@@ -1,9 +0,0 @@
-// Récupérez le bouton et l'élément de message par leur ID
-const helloButton = document.getElementById("helloButton");
-const message = document.getElementById("message");
-
-// Ajoutez un gestionnaire d'événements pour le bouton
-helloButton.addEventListener("click", () => {
-    // Affichez "Hello David" dans l'élément de message
-    message.textContent = "Hello David";
-});
